5ce1d52aee8646c08fd6606a8763008f102f1ec2
mharig <none@none>
2022-02-01 16:57:33 -0400

    Maybe finished, but not tested, yet.

--- qb_dl.py
+++ qb_dl.py
@@ -10,17 +10,25 @@ __license__ = '''(c) 2022 AGPLv3'''
 
 ##### TODO
 # 
-# - Function, that gets the content of the clipboard, preferably platform independent
+# - DONE Function, that gets the content of the clipboard, preferably platform independent
 # - Function, that spawns a wget -c, parameters: URL and filepath
 # - Function, that combines an optional parameter to the qb download command, or the basename of the link, and the configured/current download directory to a target filepath
 # - Function, that executes the qb command
 # - progress indicator
 
 
-from tkinter import Tk
+from tkinter import Tk      # needed for clipboard
 import subprocess as sp
+from os import environ
+from pathlib import Path
+import sys
 
 DLCMD = ['wget', '-q', '-c']
+
+
+def qexec(_cmd):
+    with open(environ['QUTE_FIFO'], 'w') as f:
+        f.write(_cmd)
 
 
 def getClipboard():
@@ -35,18 +43,29 @@ def getClipboard():
     return result
 
 
-def spawnDownload(_cmd, _url, _filepath):
+def spawnCmd(_cmd):
     pid = sp.Popen(_cmd, stdout=sp.PIPE, stderr=sp.PIPE)
     out, err = pid.communicate()
 
     if err.decode() != '':
-        print('error:', err.decode())
-    # TODO
-
-
-def getTargetPath(_param, _outDir, _url):
-    pass        # TODO
+        qexec(f'message-info "{err.decode()}"')
+    else:
+        qexec(f'message-info "File {_cmd[5]} downloaded."')
+    # TODO: progress bar?
 
 
 def download():
-    pass        # TODO
+    url = getClipboard()
+    tn = url.split('/')[-1]
+    tp = Path(environ['QUTE_DOWNLOAD_DIR']).joinpath(tn)
+    cmd = DLCMD.copy()
+    cmd.append(url.center(len(url)+2, '"'))     # quote
+    cmd.append('-O')
+    cmd.append(tp.center(len(tp)+2, '"'))       # quote
+
+    spawnCmd(cmd)
+
+
+if __name__ == '__main__':
+    download()
+
